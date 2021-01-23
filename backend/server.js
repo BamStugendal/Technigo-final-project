a3661ae9a00dce071a309bd8c1eaba263d82a3e8
BamStugendal <63157190+BamStugendal@users.noreply.github.com>
2021-01-23 20:06:33 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/test'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+//Mongoose model
+const Test = mongoose.model('Test', {})
+
+//   PORT=9000 npm start
+const port = process.env.PORT || 8080
+const app = express()
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors())
+app.use(bodyParser.json())
+
+//Routes
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
+
